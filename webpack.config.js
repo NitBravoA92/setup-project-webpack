61ea5232eacc19bd80ec3616f3ecc9c5cd67746a
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-06-06 14:06:54 -0400

    Add development mode in the webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   devServer: {
     static: './dist',
@@ -11,7 +12,7 @@ module.exports = {
     }),
   ],
   output: {
-    filename: 'main.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
   },
   optimization: {
